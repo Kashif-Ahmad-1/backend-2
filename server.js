6b7d0e99cb62d63e987432708476406188a34c78
Kashif Ahmad <19bcs2983@gmail.com>
2024-09-27 12:06:52 +0530

    link change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: 'http://localhost:3000', // Your frontend URL
-    origin: 'carservice-frontend-1i3i.vercel.app', // Your frontend URL
+    origin: 'https://carservice-frontend-1i3i.vercel.app/', // Your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // If you need to send cookies
 }));
